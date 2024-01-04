2aa0f6d8b0aef2198f6aea7932cd93a4933024b5
Joel <jxh1244@student.bham.ac.uk>
2024-01-04 00:39:39 +0000

    Added password encryption.

--- src/app/_lib/dataService.tsx
+++ src/app/_lib/dataService.tsx
@@ -17,8 +17,6 @@ export async function fetchUser(email: string): Promise<User | undefined> {
   return json;
 }
 
-
-
 export async function fetchPuzzle(): Promise<Puzzle | undefined> {
     const res = await fetch('http://localhost:8080/api/puzzles')
     // The return value is *not* serialized
